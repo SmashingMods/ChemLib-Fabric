4e5abfe8dfd36915c0e7abcb9c6ba2c145309df1
TechnoVisionDev <thomasanthonypeters@gmail.com>
2022-07-22 21:09:53 -0700

    Initial commit

---
+++ src/main/java/com/technovision/chemlib/ChemLib.java
@@ -0,0 +1,20 @@
+package com.technovision.chemlib;
+
+import net.fabricmc.api.ModInitializer;
+
+/**
+ * Mod library that adds periods elements
+ * and compounds as items in Minecraft.
+ *
+ * @version 1.0.0
+ * @author TecnoVisions
+ */
+public class ChemLib implements ModInitializer {
+
+    public static final String MOD_ID = "chemlib";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
