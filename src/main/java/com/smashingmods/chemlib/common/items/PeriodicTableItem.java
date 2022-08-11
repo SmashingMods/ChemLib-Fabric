526249ad5920d67521e5931cc38f0e3499f5fe6d
TechnoVisionDev <thomasanthonypeters@gmail.com>
2022-08-10 17:31:18 -0700

    Fix issue with server side loading periodic table screen classes

--- src/main/java/com/smashingmods/chemlib/common/items/PeriodicTableItem.java
+++ src/main/java/com/smashingmods/chemlib/common/items/PeriodicTableItem.java
@@ -1,15 +1,15 @@
 package com.smashingmods.chemlib.common.items;
 
-import com.mojang.logging.annotations.MethodsReturnNonnullByDefault;
 import com.smashingmods.chemlib.client.PeriodicTableScreen;
 import com.smashingmods.chemlib.registry.ItemRegistry;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
@@ -17,11 +17,8 @@ import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
 
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
 public class PeriodicTableItem extends Item {
 
     public PeriodicTableItem() {
@@ -29,16 +26,17 @@ public class PeriodicTableItem extends Item {
     }
 
     @Override
+    @Environment(EnvType.CLIENT)
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         if (world.isClient()) {
             MinecraftClient.getInstance().setScreen(new PeriodicTableScreen());
         }
-        return  TypedActionResult.success(user.getStackInHand(hand));
+        return TypedActionResult.success(user.getStackInHand(hand));
     }
 
     @Override
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
         super.appendTooltip(stack, world, tooltip, context);
-        tooltip.add(Text.translatable("item.chemlib.periodic_table.tooltip").setStyle(Style.EMPTY.withColor(Formatting.DARK_AQUA)));
+        tooltip.add(Text.translatable("item.chemlib.periodic_table.tooltip").formatted(Formatting.DARK_AQUA));
     }
 }
